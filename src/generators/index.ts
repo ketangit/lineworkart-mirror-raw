437e126edb7047479355ed6f374b5cb6b5c6fc51
Ketan <ketangit@users.noreply.github.com>
2026-07-23 11:54:16 +0530

    feat: register flow-field generator

--- src/generators/index.ts
+++ src/generators/index.ts
@@ -3,8 +3,10 @@
 import { registerGenerator } from "../core/registry";
 import { spirograph } from "./spirograph";
 import { rose } from "./rose";
+import { flowField } from "./flow-field";
 
 export function registerGenerators(): void {
   registerGenerator(spirograph);
   registerGenerator(rose);
+  registerGenerator(flowField);
 }
