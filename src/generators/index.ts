3c869f91ffdd42ff04f12dbd0a846f6ef6ba870b
Ketan <ketangit@users.noreply.github.com>
2026-07-23 11:59:07 +0530

    feat: register truchet generator

--- src/generators/index.ts
+++ src/generators/index.ts
@@ -4,9 +4,11 @@ import { registerGenerator } from "../core/registry";
 import { spirograph } from "./spirograph";
 import { rose } from "./rose";
 import { flowField } from "./flow-field";
+import { truchet } from "./truchet";
 
 export function registerGenerators(): void {
   registerGenerator(spirograph);
   registerGenerator(rose);
   registerGenerator(flowField);
+  registerGenerator(truchet);
 }
